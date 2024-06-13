77111fc90727b464ab93ae2f6be02fb535763cc5
Ankit Chouhan <99067327+Chouhanankit@users.noreply.github.com>
2024-06-13 11:06:28 +0530

    Update authService.js

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,13 +1,13 @@
 import axios from "axios"
 
 const login = async (formData) => {
-    const response = await axios.post("https://supportbackend-vevi.onrender.com/api/user/login", formData)
+    const response = await axios.post("https://supportdesknew.onrender.com/api/user/login", formData)
     localStorage.setItem("user", JSON.stringify(response.data));
     return response.data
 }
 
 const register = async (formData) => {
-    const response = await axios.post("https://supportbackend-vevi.onrender.com/api/user", formData)
+    const response = await axios.post("https://supportdesknew.onrender.com/api/user", formData)
     return response.data
 }
 
